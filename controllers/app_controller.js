13b28289a40e6bc8b6849c8f12460bc6b5c0b792
Alex Wander <alexcwander@gmail.com>
2020-06-03 12:43:45 -0500

    add simple styling to landing page and link to index

--- controllers/app_controller.js
+++ controllers/app_controller.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express.Router();
 
+// Landing Page
 app.get("/", (req, res) => {
   res.render("../views/main/landing.ejs");
+});
+
+// Home Page
+app.get("/home", (req, res) => {
+  res.render("../views/main/index.ejs");
 });
 
 module.exports = app;
