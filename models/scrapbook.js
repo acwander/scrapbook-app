f62e97f7a766b03c8246d94b935e636148273577
Alex Wander <alexcwander@gmail.com>
2020-06-03 14:28:17 -0500

    create scrapbook model

---
+++ models/scrapbook.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+var scrapbookSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1535954741680-a2e24eb05418?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Export the model
+module.exports = mongoose.model("Scrapbook", scrapbookSchema);
